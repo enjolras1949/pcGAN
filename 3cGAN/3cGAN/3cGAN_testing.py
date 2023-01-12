532be4458548ecaa57529e6eb601043183e7cc81
Lu Guo <45565626+enjolras1949@users.noreply.github.com>
2023-01-12 19:04:11 +0100

    Update 3cGAN_testing.py

--- 3cGAN/3cGAN/3cGAN_testing.py
+++ 3cGAN/3cGAN/3cGAN_testing.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 12:37:43 2023
+
+@author: lg809
+"""
+
 import argparse
 from torch.utils.data import DataLoader
 from models import *
@@ -6,16 +14,14 @@ from utils import *
 import torch
 print(torch.__version__)
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="3ppGAN")
 
-parser.add_argument("-network_name", type=str, default="3cGAN", help="name of the network")
-parser.add_argument("--dataset_name", type=str, default="ex-vivo", help="name of the training dataset")
+parser.add_argument("-network_name", type=str, default="3ppGAN", help="name of the network")
+parser.add_argument("--training_dataset", type=str, default="ex-vivo", help="name of the dataset")
 parser.add_argument("--testing_dataset", type=str, default="ex-vivo", help="name of the testing dataset")
-parser.add_argument("--lambda_cyc", type=float, default=0.1, help="cycle loss weight")
 
-
-parser.add_argument("--epoch", type=int, default=50, help="epoch to start training from")
-parser.add_argument("--n_epochs", type=int, default=51, help="number of epochs of training")
+parser.add_argument("--epoch", type=int, default=0, help="epoch to start training from")
+parser.add_argument("--n_epochs", type=int, default=51, help="number of epochs oef training")
 parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
@@ -27,32 +33,33 @@ parser.add_argument("--img_width", type=int, default=200, help="size of image wi
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=1, help="interval between saving generator outputs")
 parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between saving model checkpoints")
+
+
+parser.add_argument("--lambda_merging", type=float, default=10, help="scaling factor for the new loss")
 parser.add_argument("--textfile_training_results_interval", type=int, default=50, help="textfile_training_results_interval")
-parser.add_argument("--n_residual_blocks", type=int, default=9, help="number of residual blocks in generator")
-parser.add_argument("--lambda_id", type=float, default=1, help="identity loss weight")
+
 opt = parser.parse_args()
 print(opt)
 
 # Losses
 criterion_GAN = torch.nn.MSELoss()
-criterion_cycle = torch.nn.MSELoss()
-criterion_identity = torch.nn.L1Loss(size_average=None, reduce=None, reduction = 'elementwise_mean')
+criterion_pixelwise = torch.nn.L1Loss()
 
 cuda = torch.cuda.is_available()
 input_shape = (opt.channels, opt.img_height, opt.img_width)
 # Initialize generator and discriminator
-G_AB = GeneratorResNet(input_shape, opt.n_residual_blocks)
+G_AB = GeneratorUNet()
 
 if cuda:
     G_AB = G_AB.cuda()
 
-G_AB.load_state_dict(torch.load("saved_models/%s-%s-G_AB-%dep.pth" % (opt.network_name, opt.dataset_name, opt.epoch)))
+G_AB.load_state_dict(torch.load("saved_models/%s-%s-G_AB-%dep.pth" % (opt.network_name, opt.testing_dataset, opt.epoch)))
 
 Tensor = torch.cuda.FloatTensor if cuda else torch.Tensor
 
 # Buffers of previously generated samples
-fake_B1_buffer = ReplayBuffer()
-fake_A2_buffer = ReplayBuffer()
+fake_AB_buffer = ReplayBuffer()
+
 
 transforms_testing_non_fliped_ = [
     transforms.ToTensor(),
@@ -72,8 +79,6 @@ def testing():
 
     for i, batch in enumerate(val_dataloader_non_flipped):
         real_A = Variable(batch["A"].type(Tensor))
-        fake_B1 = G_AB(real_A)
-        save_image(fake_B1, "images/%s-Est-Depths/%s-Est-Depths-%s.png" % (opt.network_name,opt.network_name, i),normalize=False, scale_each=False) #range= (0,128)
+        fake_AB = G_AB(real_A)
+        save_image(fake_AB, "images/%s-Est-Depths/%s-Est-Depths-%s.png" % (opt.network_name,opt.network_name, i),normalize=False, scale_each=False) #range= (0,128)
 testing()
-
-
